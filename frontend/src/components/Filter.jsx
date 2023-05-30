9ecbd99d710698775c75ea0d6419c8adb2187259
vcalomino4 <134813403+vcalomino4@users.noreply.github.com>
2023-05-30 11:27:53 -0300

    Delete frontend directory

--- frontend/src/components/Filter.jsx
+++
@@ -1,6 +0,0 @@
-const Filter = (props) => (
-  <div>
-    <input value={props.value} onChange={props.onChange} />
-  </div>
-);
-export default Filter;
--- frontend/src/components/Notification.jsx
+++
@@ -1,8 +0,0 @@
-const Notification = (props) => {
-  if (props.message === null) {
-    return null;
-  }
-
-  return <div className={props.className}>{props.message}</div>;
-};
-export default Notification;
